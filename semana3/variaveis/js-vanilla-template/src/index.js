93f3b47d6ef783feace12ca4d522395b0f6c515a
patricia-abreu <patricia.otarao@hotmail.com>
2020-09-28 17:25:47 -0300

    Exercícios parte 01

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,12 @@
+let arrays = ["pipoca doce", "pizza", "açaí", "bolo de morango e chantily", "canelone"]
+
+Array [1] = "pipoca doce"
+Array [2] = "pizza"
+Array [3] = "açaí"
+Array [4] = "bolo de morango e chantily"
+Array [5] = "canelone"
+
+
+prompt("Qual sua comida preferida?", "Resposta")
+
+Array [2] = Resposta
