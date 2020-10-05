28a8bba34e3e6700a4f1090b4db7d1cdacd8186d
patricia-abreu <patricia.otarao@hotmail.com>
2020-10-04 23:07:52 -0300

    nossoJogo e desafio 8

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -11,3 +11,36 @@
  * 
  */
 
+// DESAFIO 8
+
+let user = []
+let pc = []
+
+console.log("Bem vindo ao jogo de Blackjack!")
+
+let iniciarRodada = confirm("Quer iniciar uma nova rodada?")
+
+   let userCarta1 = comprarCarta()
+   let userCarta2 = comprarCarta()
+   let pcCarta1 = comprarCarta()
+   let pcCarta2 = comprarCarta()
+
+   if (userCarta1.valor !== 11 && userCarta2.valor !== 11) {
+      user.push(userCarta1)
+      user.push(userCarta2)
+      } else {
+      userCarta1 = comprarCarta()
+      userCarta2 = comprarCarta()
+      user.push(userCarta1)
+      user.push(userCarta2)
+      }
+
+   if (pcCarta1.valor !== 11 && pcCarta2.valor !== 11){
+      pc.push(pcCarta1)
+      pc.push(pcCarta2)
+      } else {
+      pcCarta1 = comprarCarta()
+      pcCarta2 = comprarCarta()
+      pc.push(pcCarta1)
+      pc.push(pcCarta2)
+      }
--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -11,33 +11,40 @@
  * 
  */
 
+//Mensagem inicial
 console.log("Bem vindo ao jogo de Blackjack!")
 
-const inicioJogo = confirm("Quer iniciar uma nova rodada?")
+let user = []
+let pc = []
 
-let cartasUser = []
-let pontosUser = []
-let PrintCartasUser = ""
-let printPontosUser = 0
 
-let cartaPc =[]
-let pontosPc = []
-let printCartasPc = ""
-let printPontosPc = 0
+let inicioJogo = confirm("Quer iniciar uma nova rodada?")
 
-if(inicioJogo) {
-   for (let i = 0; i<2;) {
-      let carta = comprarCarta()
-      cartasUser.push(carta.texto)
-      pontosUser.push(carta.valor)
-      if ((cartasUser[0] === "A") && (cartasUser[1] ==="A")){
-         i = i
-      } else {
-         i++
-      }
-      }
-   }
+//Iniciar uma rodada.
+if(inicioJogo){
+   
+   let userCarta1 = comprarCarta()
+   let userCarta2 = comprarCarta()
+   let pcCarta1 = comprarCarta()
+   let pcCarta2 = comprarCarta()
 
-   for (let carta of cartasUser) {
-      printPontosUser += (carta + " ")
+
+   // Imprime no console as cartas(naipe + número) e pontuação do Usuário e Computador.
+   console.log("Usuário - cartas: " + userCarta1.texto + " " + userCarta2.texto + " - pontuação " + Number(userCarta1.valor + userCarta2.valor) +".")
+   console.log("Computador - cartas: " + pcCarta1.texto + " " + pcCarta2.texto + " - pontuação " + Number(pcCarta1.valor + pcCarta2.valor) + ".")
+
+
+   //Guarda a pontuação da soma das 2 cartas sorteadas para cada jogador.
+   let userPoints = userCarta1.valor + userCarta2.valor
+   let pcPoints = pcCarta1.valor + pcCarta2.valor
+
+   if (userPoints === pcPoints) {
+      console.log("Empate!")  // Imprime no console "Empate!" se os pontos do Usuário e Computador forem iguais.
+   } else if (userPoints>pcPoints) {
+      console.log("O usuário ganhou!") // Imprime 'O usuário ganhou!' se os pontos do Usuário for maior que do Computador.
+   } else {console.log("O computador ganhou!")} // Imprime 'O computador ganhou!' se os pontos do Computador for maior que do Usuário.
+
+   // Se a resposta do 'confirm' inicial for 'cancelar', o console imprime a mensagem 'O jogo acabou!'.
+   } else {
+   console.log("O jogo acabou!")
    }
