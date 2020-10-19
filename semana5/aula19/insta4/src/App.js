366fd0b64f6b9aad1c961c1635fb5117a0876d49
patricia-abreu <patricia.otarao@hotmail.com>
2020-10-18 22:13:51 -0300

    Implementação insta4

--- semana5/aula19/insta4/src/App.js
+++ semana5/aula19/insta4/src/App.js
@@ -1,31 +1,96 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    posts: [
+      {
+        nomeUsuario: "Paulinha",
+        fotoUsuario: "https://picsum.photos/id/1027/200",
+        fotoPost: "https://picsum.photos/id/1008/500"
+      },
+      {
+        nomeUsuario: "Carlos",
+        fotoUsuario: "https://picsum.photos/id/447/200",
+        fotoPost: "https://picsum.photos/id/1025/500"
+      },
+      {
+        nomeUsuario: "Ana",
+        fotoUsuario: "https://picsum.photos/id/342/200",
+        fotoPost: "https://picsum.photos/id/1023/500"
+      }
+    ],
+    valorInputNomeUsuario: "",
+    valorInputFotoUsuario: "",
+    valorInputFotoPost: ""
+  };
+
+  addPost = () => {
+    const novoPost = {
+      nomeUsuario: this.state.valorInputNomeUsuario,
+      fotoUsuario: this.state.valorInputFotoUsuario,
+      fotoPost: this.state.valorInputFotoPost
+    };
+
+    const novoArrayPosts = [novoPost, ...this.state.posts];
+
+    this.setState({ posts: novoArrayPosts });
+    this.setState({ valorInputNomeUsuario: "" });
+    this.setState({ valorInputFotoUsuario: "" });
+    this.setState({ valorInputFotoPost: "" });
+  };
+
+  onChangeInputNomeUsuario = (event) => {
+    this.setState({ valorInputNomeUsuario: event.target.value });
+  };
+
+  onChangeInputFotoUsuario = (event) => {
+    this.setState({ valorInputFotoUsuario: event.target.value });
+  };
+
+  onChangeInputFotoPost = (event) => {
+    this.setState({ valorInputFotoPost: event.target.value });
+  };
+
   render() {
+    const listaDePosts = this.state.posts.map((post) => {
+      return (
+        <Post
+          nomeUsuario={post.nomeUsuario}
+          fotoUsuario={post.fotoUsuario}
+          fotoPost={post.fotoPost}
+        />
+      );
+    });
+
     return (
       <div className={'app-container'}>
-      <Post
-      nomeUsuario={'Paulinha'}
-      fotoUsuario={'https://picsum.photos/id/1027/200'}
-      fotoPost={'https://picsum.photos/id/1008/500'}
-      />
-      <Post
-      nomeUsuario={'Carlos'}
-      fotoUsuario={'https://picsum.photos/id/447/200'}
-      fotoPost={'https://picsum.photos/id/1025/500'}
-      />
-      <Post
-      nomeUsuario={'Ana'}
-      fotoUsuario={'https://picsum.photos/id/342/200'}
-      fotoPost={'https://picsum.photos/id/1023/500'}
-      />
+        <div>
+          <input
+            value={this.state.valorInputNomeUsuario}
+            onChange={this.onChangeInputNomeUsuario}
+            placeholder={"Nome do usuário"}
+          />
+          <input
+            value={this.state.valorInputFotoUsuario}
+            onChange={this.onChangeInputFotoUsuario}
+            placeholder={"Foto do perfil"}
+          />
+          <input
+            value={this.state.valorInputFotoPost}
+            onChange={this.onChangeInputFotoPost}
+            placeholder={"Foto do post"}
+          />
+          <button onClick={this.addPost}>Adicionar</button>
+        </div>
+        <div>
+          {listaDePosts}
+        </div>
       </div>
     );
   }
 }
-
-
 
 export default App;
